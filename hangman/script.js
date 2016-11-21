ed1b53cb42844880e7b425d24bd591a1a8d9d02a
Kathleen <kathleenbryan24@gmail.com>
2016-11-21 13:50:28 -0800

    hangman changes

--- hangman/script.js
+++ hangman/script.js
@@ -1,30 +1,32 @@
 var array = [];
 var guess = [];
-var win = []; 
-var lives = 10;
+var win = [];
+var life = 10;  
 var pat = /^[a-z]+$/;
 
 function wordObject() {
-  this.word = " ";
-  this.letter = " "; 
+  this.word = "";
+  this.letter = ""; 
   this.length = 0;
+ 
 }
 
 var myword = new wordObject;
 
 var div = document.getElementById("placeholder");
-var lifecount = document.createTextNode(lives);
+var lifecount = document.createTextNode(life);
 div.appendChild(lifecount);
 
 function checkword() {
-  var usrtext = document.getElementsByName('textbox1')[0].value
+  var usrtext = this.letter; 
   if(pat.test(usrtext) == false){
   alert('Only lowercase characters allowed');
   } else {
   getword();
   makeblanks(); 
-  win.push(usrtext.length);
+  win.push(myword.length);
   }
+
 }
 
 function getword() {
@@ -32,6 +34,7 @@ function getword() {
   var textlength = text.length; 
   myword.word = text;
   myword.length = textlength; 
+  document.getElementsByName('textbox1')[0].value="";
 }
  
 function makeblanks() {
@@ -48,10 +51,9 @@ for(i=0; i< array.length; i++) {
 
 function checkuserletter(x) {
     var num = x;
-    var number = num + 1;
     var textnode = document.createTextNode(myword.letter);
     var item = document.getElementById("placeholderb");
-    item.replaceChild(textnode, item.childNodes[number]); 
+    item.replaceChild(textnode, item.childNodes[num]); 
     win[0] = win[0] - 1 
     if (win[0] == 0) {
     alert("you won!");
@@ -71,6 +73,7 @@ function checkletter() {
 function getletter() {
   var letter = document.getElementsByName('textbox2')[0].value
   myword.letter = letter;
+  document.getElementsByName('textbox2')[0].value="";
 }
 
 function pagedisplay() {
@@ -94,21 +97,22 @@ var check = guess.indexOf(myword.letter);
 
 function lettertest() {
   var i = -1;
-  var check = myword.word.indexOf(myword.letter);
+  var test = myword.word
+  var check = test.indexOf(myword.letter);
   if (check!= -1) {
-  while ((i = myword.word.indexOf(myword.letter, i+1)) != -1) {
+  while ((i = test.indexOf(myword.letter, i+1)) != -1) {
   checkuserletter(i); }
-  } else {
-  lives = lives -1;
-    if (lives == 0) {
+  } else { 
+ life = life -1;
+    if (life == 0) {
         alert("you loose");
-        var textnode = document.createTextNode(lives);
+        var textnode = document.createTextNode(life);
         var item = document.getElementById("placeholder");
-        item.replaceChild(textnode, item.childNodes[1]); 
+        item.replaceChild(textnode, item.childNodes[0]); 
     } else {
-        var textnode = document.createTextNode(lives);
+        var textnode = document.createTextNode(life);
         var item = document.getElementById("placeholder");
-        item.replaceChild(textnode, item.childNodes[1]); 
+        item.replaceChild(textnode, item.childNodes[0]); 
       }
     } 
 }
